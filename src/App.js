09afdf5007c1a736592c375eaf1e8e37e6b149b9
Max <mboyar54@gmail.com>
2020-12-23 20:27:00 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import Coin from './Coin';
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+
+
+
 
 function App() {
+  const [coins,setCoins]=useState([]);
+  const [search,setSearch]=useState('');
+
+  /*
+  1.fetch api with useEffect
+  2.filtered coins wil lhold filtered coins according to searhbox
+  3.map filterdCoins and send props to Coin component and return <Coin/>
+  */
+
+  //https://34981.wayscript.io/
+  //https://api.coingecko.com/api/v3/coins/markets?vs_currency=ils&order=market_cap_desc&per_page=100&page=1&sparkline=false
+  //Fetch Api only once when component did mount (App)
+  useEffect(()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=ils&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res=>{
+      console.log(res)
+      setCoins(res.data)
+      
+    }).catch(err=>{
+      console.log(err)
+    })
+  },[])
+
+  
+  //We call setSearch with a new value. React will then re-render thecomponent, passing the new search value to it.
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  //FilteredCoins holds filtered arr according to "search" value in searchBox
+  const filteredCoins = coins.filter(coin=>{
+    return(coin.name.toLowerCase().includes(search.toLowerCase()))
+  }
+    
+  )
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+      <div className="coin-app">
+        <h1 className=" logo text-center mb-4">Crypto currency App</h1>
+        <div className="text-center coin-search mb-5">
+          <Row className="justify-content-center">
+            <h5 className="coin-text mr-2">Search a currency:</h5>
+            <form>
+            <input type='text' placeholder='Search' className='coin-input' onChange={handleChange}>
+            </input>
+            
+          </form>
+          </Row>
+          
+          
+        </div>
+
+      {filteredCoins.length===0 ? <h4 style={{textAlign:"center"}}>Sorry no data found for the Query...</h4>:filteredCoins.map(coin=>{
+        return(
+          
+          
+          
+            <Coin 
+          key={coin.id}
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          />
+        )
+      })}
+      
+      {/* {filteredCoins.map(coin=>{
+        return(
+          
+          
+          
+            <Coin 
+          key={coin.id}
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          />
+        )
+      })} */}
     </div>
+  </Container>
   );
 }
 
---
+++ src/Coin.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+//rface -> creat arrow function
+
+import './Coin.css'
+
+const Coin = ({name,image,symbol,price,volume,priceChange}) => {
+    return (
+       
+             <div className='coin-container'>
+            <div className='coin-row'>
+                <Row>
+                
+                    <img src={image} alt="img" className="img"></img>
+                    <Col><h1>{name}</h1></Col>
+                    <Col><p className='coin-symbol'>{symbol}</p></Col>
+                   
+                
+                
+                    <Col><p className="coin-price">ILS: {price.toLocaleString()} </p></Col>
+                    <Col><p className="coin-volume">{volume.toLocaleString()}</p></Col>
+                    <Col>{priceChange> 0 ? <p style={{color:"green"}}>+{priceChange} %</p>:<p style={{color:"red"}}>{priceChange} %</p>}
+</Col>
+                
+
+                </Row>
+                
+
+            </div>
+            
+        </div>
+
+        
+       
+    )
+}
+
+export default Coin;
